f61abc565d699d85a765b9244387405765f59efc
Dominique Sakrisson <domsakrisson@gmailcom>
2021-03-04 22:29:41 -0800

    CHARACTER FILTERED QUOTES ENDPOINT DONE

---
+++ data/characters.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    _id: '5cd99d4bde30eff6ebccfea0',
+    name: 'Gandalf',
+  },
+  {
+    _id: '5cd99d4bde30eff6ebccfd81',
+    name: 'Legolas',
+  },
+  {
+    _id: '5cd99d4bde30eff6ebccfcc8',
+    name: 'Elrond',
+  },
+  {
+    _id: '5cd99d4bde30eff6ebccfc15',
+    name: 'Frodo Baggins',
+  }
+];
---
+++ data/favorites.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    _db_quote_id: '5cd96e05de30eff6ebcce8e3',
+    dialog: 'One that is cursed. Long ago the men of the mountains swore an oath to     the last King of Gondor , to come to his aid, to fight. But when     the time came, when Gondor\'s need was dire, they fled vanishing into the     darkness of the mountain. And so Isildur cursed them, never to rest until     they had fulfilled their pledge. Who shall call them from the grey twilight,     the forgotten people? The heir of him to whom the oath they swore. From the     North shall he come, need shall drive him. He shall pass the door to the     Paths of the Dead.  ',
+    character: '5cd99d4bde30eff6ebccfd81'
+  },
+  {
+    _db_quote_id: '5cd96e05de30eff6ebcce8e7',
+    dialog: 'The way is shut. It was made by those who are dead , and the dead keep it. The     way is shut.  ',
+    character: '5cd99d4bde30eff6ebccfd81'
+  },
+  {
+    _db_quote_id: '5cd96e05de30eff6ebcce900',
+    dialog: 'I see shapes of men and of horses.',
+    character: '5cd99d4bde30eff6ebccfd81'
+  }
+];
